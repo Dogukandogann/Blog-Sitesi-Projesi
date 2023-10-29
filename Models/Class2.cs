5aa908fdab305b65f9ab9848ea371bd159d57a3d
Doğukan Doğan <flptdogukan@hotmail.com>
2023-10-29 20:56:55 +0300

    add GoogleCharts

---
+++ Models/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogProjesi.Models
+{
+    public class Class2
+    {
+        public int BlogRating { get; set; }
+        public string  BlogName { get; set; }
+    }
+}
