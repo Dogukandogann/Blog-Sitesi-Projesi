2387a13420d4fdde034348ceeff62b91b9df75d9
Doğukan Doğan <flptdogukan@hotmail.com>
2023-10-29 23:59:14 +0300

    add AuthorModeleDetails

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BlogProjesi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult AuthorLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AuthorLogin(Author p)
+        {
+            Context c = new Context();
+            var userInfo=c.Authors.FirstOrDefault(x=>x.Mail==p.Mail && x.Password==p.Password);
+            if (userInfo!=null)
+            {
+                FormsAuthentication.SetAuthCookie(userInfo.Mail, false);
+                Session["Mail"] = userInfo.Mail.ToString();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return RedirectToAction("AuthorLogin", "Login");
+            }
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProjesi.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: AuthorLogin
+        UserProfileManager _userProfileManager = new UserProfileManager();
+
+        public ActionResult Index()
+        {
+           return View();
+        } 
+        public PartialViewResult Partial1(string p)
+        {
+            p = (string)Session["Mail"];
+            var profileValues = _userProfileManager.GetAuthorByMail(p);
+            return PartialView(profileValues);
+        }
+        public ActionResult BlogList(string p)
+        {
+            p = (string)Session["Mail"];
+            Context c = new Context();
+            int id = c.Authors.Where(x => x.Mail == p).Select(x => x.AuthorId).FirstOrDefault();
+            var blogs = _userProfileManager.GetBlogByAuthor(id);
+            return View(blogs);
+        }
+    }
+    
+}
